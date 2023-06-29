e23cffd57ca9a28c3d1939b364a4673cfa0798e7
Ethorcharl <zenja_riga5@yahoo.com>
2023-06-29 12:48:53 +0300

    CORS settings

--- JwtWebApiRegistrationNet6/Program.cs
+++ JwtWebApiRegistrationNet6/Program.cs
@@ -40,6 +40,13 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme) //it 
         };
     });
 
+//enable CORS. This is need for frontend angular
+builder.Services.AddCors(options => options.AddPolicy(name: "NgOrigins",
+    policy => 
+    {
+        policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+    }));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -48,6 +55,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("NgOrigins");
 
 app.UseHttpsRedirection();
 
